eb314ffd96b577b5ac53a02219749e75805f9915
kaitlyn ziarkowski <kaitlyn.ziarkowski@gmail.com>
2026-08-26 10:18:24 -0700

    Add API URL

--- docs/config.js
+++ docs/config.js
@@ -3,7 +3,7 @@
  * It looks like: https://script.google.com/macros/s/AKfycb..../exec
  */
 window.JOBS_CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbyBS4uVSKPS2tnldo5P8Oo2mexwZaT_2VSXqPDlapbWvE5L-8UDBUzh9f4onXqOLjgl/exec",
 
   // Only needed if you set CONFIG.API_KEY in the Apps Script project.
   API_KEY: ""
